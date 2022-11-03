ad7503150c46a6fce682ba68e7dadc73d1a62f7d
Joon Park <jsp220@gmail.com>
2022-11-02 23:01:53 -0700

    complete portfolio page with projects

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Project from './Project';
 import projOne from '../../../assets/images/proj1.png';
-
+import projTwo from '../../../assets/images/proj2.png';
+import projThree from '../../../assets/images/proj3.png';
+import projFour from '../../../assets/images/proj4.png';
+import projFive from '../../../assets/images/proj5.png';
+import projSix from '../../../assets/images/proj6.png';
 
 
 const Portfolio = () => {
@@ -13,30 +17,35 @@ const Portfolio = () => {
 			img: projOne
 		},
 		{
-			name: "Corgis Messenger",
-			link: "https://corgi-messenger.herokuapp.com/",
-			img: projOne
+			name: "Fullstech Blog",
+			link: "https://fullstech-blog.herokuapp.com/",
+			repo: "https://github.com/jsp220/Fullstech-Blog",
+			img: projTwo
 		},
-		// {
-		// 	name: "Corgis Messenger",
-		// 	link: "https://corgi-messenger.herokuapp.com/",
-		// 	img: projTwo
-		// },
-		// {
-		// 	name: "Corgis Messenger",
-		// 	link: "https://corgi-messenger.herokuapp.com/",
-		// 	img: projTwo
-		// },
-		// {
-		// 	name: "Corgis Messenger",
-		// 	link: "https://corgi-messenger.herokuapp.com/",
-		// 	img: projTwo
-		// },
-		// {
-		// 	name: "Corgis Messenger",
-		// 	link: "https://corgi-messenger.herokuapp.com/",
-		// 	img: projTwo
-		// },
+		{
+			name: "Work Day Scheduler",
+			link: "https://jsp220.github.io/Work-Day-Scheduler/",
+			repo: "https://github.com/jsp220/Work-Day-Scheduler",
+			img: projThree
+		},
+		{
+			name: "Weather Dashboard",
+			link: "https://jsp220.github.io/Weather-Dashboard/",
+			repo: "https://github.com/jsp220/Weather-Dashboard",
+			img: projFour
+		},
+		{
+			name: "Employee Tracker",
+			link: "https://github.com/jsp220/Employee-Tracker",
+			repo: "https://github.com/jsp220/Employee-Tracker",
+			img: projFive
+		},
+		{
+			name: "Team Profile Generator",
+			link: "https://github.com/jsp220/Team-Profile-Generator",
+			repo: "https://github.com/jsp220/Team-Profile-Generator",
+			img: projSix
+		},
 	]
 	
 	return (
