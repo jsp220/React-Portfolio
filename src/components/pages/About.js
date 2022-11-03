56a0fda352b17ba78cc00b320ad29a1fb622a0f2
Joon Park <jsp220@gmail.com>
2022-11-02 22:47:47 -0700

    add photo to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Me from '../../assets/images/me.jpg';
 
 export default function AboutMe() {
 	return (
 		<div className="pt-1">
 			<h1 className="pb-1">About Me</h1>
+			<img src={Me} className="me-photo"/>
 			<p>
 				Joon Park is a professional full stack developer based in San Diego, CA. He completed his certificate for full stack web development at UC San Diego Extended Studies in November 2022. Joon has developed various frontend, backend, and full stack applications, some of which can be found on the Portfolio page.
 			</p>
