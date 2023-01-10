db613645cb31c3ab01b5d53fafed4821bb301438
Joon Park <jsp220@gmail.com>
2023-01-09 21:53:34 -0800

    fix contact me form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 	// use state for all 3 fields in the form
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [message, setMessage] = useState('');
-	
+
 	// update state every time there is a change in any of the fields
 	const handleInputChange = (e) => {
 		// Getting the value and name of the input which triggered the change
 		const { name, value } = e.target;
-		
+
 		if (name == "name") return setName(value);
 		if (name == "email") return setEmail(value);
 		if (name == "message") return setMessage(value);
@@ -42,51 +43,66 @@ export default function Contact() {
 			alert("Please enter all required fields.");
 			return;
 		}
-		alert("Thank you for your message!");
-		// set all fields back to blank
-		setName('');
-		setEmail('');
-		setMessage('');
-		return;
+
+		const templateParams = {
+			from_name: name,
+			reply_to: email,
+			message: message
+		};
+
+		emailjs.send("service_74i9fmx", "template_p71o2bq", templateParams, "WNKsKHM2pWy83-JW-")
+			.then((response) => {
+				alert("Thank you for your message!");
+				// console.log(response.text);
+				setName('');
+				setEmail('');
+				setMessage('');
+				return;
+			}, (error) => {
+				alert("That didn't work. Please try again.");
+				// console.log(error);
+				return;
+			});
+
 	}
-	
+
 	return (
 		<div className="pt-1">
 			<h1 className="pb-1">Contact Me</h1>
 			<form id="contact-form">
 				<div className="mb-3">
 					<label for="contact-name" className="form-label">Name</label>
-					<input 
+					<input
 						value={name}
 						name="name"
 						onChange={handleInputChange}
 						onBlur={handleBlur}
-						type="email" 
-						className="form-control" 
-						id="contact-name" 
+						type="email"
+						className="form-control"
+						id="contact-name"
 						placeholder="Your Name" />
 				</div>
 				<div className="mb-3">
 					<label for="contact-email" className="form-label">Email address</label>
-					<input 
+					<input
 						value={email}
 						name="email"
 						onChange={handleInputChange}
 						onBlur={handleBlur}
-						type="email" 
-						className="form-control" 
-						id="contact-email" 
+						type="email"
+						className="form-control"
+						id="contact-email"
 						placeholder="name@example.com" />
 				</div>
 				<div className="mb-3">
 					<label for="contact-message" className="form-label">Message:</label>
-					<textarea 
+					<textarea
 						value={message}
 						name="message"
 						onChange={handleInputChange}
 						onBlur={handleBlur}
-						className="form-control" 
-						id="contact-message" 
+						className="form-control"
+						id="contact-message"
 						rows="3"
 					></textarea>
 				</div>
@@ -95,5 +111,7 @@ export default function Contact() {
 				</div>
 			</form>
 		</div>
+
+
 	);
 }
