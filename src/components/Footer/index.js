e66102881710bbdcec2b782ca8cda1ee31d5bd49
Joon Park <jsp220@gmail.com>
2025-01-03 12:25:54 -0800

    Edit bio

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,16 +10,12 @@ function Footer() {
             name: "LinkedIn",
             link: "https://www.linkedin.com/in/joon-park-155a7424/"
         },
-        {
-            name: "Stack Overflow",
-            link: "https://stackoverflow.com/users/19662710/joon-park"
-        },
     ]
 
     return (
         <footer>
             <div className="container-fluid footer d-flex flex-wrap justify-content-between align-items-center py-2 my-2 border-top">
-                <p className="col-3 mb-0 text-muted">©2023 Joon Park</p>
+                <p className="col-3 mb-0 text-muted">©2025 Joon Park</p>
                 <ul className="col-9 nav justify-content-end">
                     {links.map((item) => {
                         return <a className="nav-link black" href={item.link} key={item.name} target="_blank" rel="noreferrer">{item.name}</a>
